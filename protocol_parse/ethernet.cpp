4d0367059f617f7af6cd79ee83d3a9ee3eaf2b0c
yhs-coder <yhs6666@outlook.com>
2024-03-28 20:37:48 +0800

    Ethernet类封装优化

--- protocol_parse/ethernet.cpp
+++ protocol_parse/ethernet.cpp
@@ -1,41 +1,55 @@
 #include "ethernet.h"
+#include "ip.h"
 
-EthernetHeader::EthernetHeader(bool is_opposite_byte)
-    : Protocol(is_opposite_byte)
+//Ethernet::Ethernet() : Protocol(false), _data(nullptr)
+Ethernet::Ethernet() : Protocol(false), _data()
 {
     memset(_preamble, ETHERNET_PREAMBLE_VALUE,ETHERNET_PREAMBLE_BYTE);
     _sfd = ETHERNET_SFD_VALUE;
     memset(_target_mac, 0 , ETHERNET_MAC_BYTE);
     memset(_source_mac, 0 , ETHERNET_MAC_BYTE);
     _type = 0;
-    _size = ETHERNET_HEADER_MIN_BYTE; 
+    _size = ETHERNET_HEADER_MIN_BYTE;
+    _is_opposite_byte = get_endian() == LITTLE_ENDIAN;
 }
 
-EthernetHeader::~EthernetHeader()
-{}
+Ethernet::~Ethernet()
+{
+   // if (_data != nullptr)
+   // {
+   //     printf("~Ethernet.......\n");
+   //     delete _data;
+   //     _data = nullptr;
+   // }
+}
 
 /*
  * @以buffer起始地址作为以太网数据包头，检查受否有前导码和帧开始符
  * @return bool 
  * */
-bool EthernetHeader::is_have_preamble_and_sfd(void *buffer, uint32_t size)
+bool Ethernet::is_have_preamble_and_sfd(void *buffer, uint32_t size)
 {
+    // 以太网帧不包含前导码和帧开始符这些物理层的内容
+    // 数据区域是以太网帧的有效载荷部分开始，也就是DST目标MAC地址开始
+    // 所以以下函数无任何意义！！！！！！！！！！！！！！！！！！！！！！！！
     if (size < ETHERNET_PREAMBLE_BYTE + ETHERNET_SFD_BYTE)
         return false;
-    u_char *cbuffer = (u_char*)buffer;
-    return (memcmp(cbuffer, _preamble, ETHERNET_PREAMBLE_VALUE) == 0) && (*(cbuffer + ETHERNET_PREAMBLE_VALUE) == _sfd);
+   // u_char *cbuffer = (u_char*)buffer;
+    //printf("ethernet.cpp line:%d\n",__LINE__);
+    return false;
+    //return (memcmp(cbuffer, _preamble, ETHERNET_PREAMBLE_BYTE) == 0) && (*(cbuffer + ETHERNET_PREAMBLE_BYTE) == _sfd);
 }
 
-bool EthernetHeader::is_protocol_type()
+bool Ethernet::is_protocol_type()
 {
     switch(_type)
     {
         // 类型定义在/usr/include/net/ethernet.h文件中
         case ETHERTYPE_IP:
+            return true;
         case ETHERTYPE_ARP:
+            return true;
         case ETHERTYPE_REVARP:
-        case ETHERTYPE_AT:
-        case ETHERTYPE_AARP:
         case ETHERTYPE_VLAN:
         case ETHERTYPE_IPV6:
         case ETHERTYPE_LOOPBACK:
@@ -45,24 +59,29 @@ bool EthernetHeader::is_protocol_type()
     }
 }
 
-bool EthernetHeader::check_buffer_length(void *buffer, uint32_t size)
+bool Ethernet::check_buffer_length(void *buffer, uint32_t size)
 {
     if (is_have_preamble_and_sfd(buffer, size))
-        return size >= ETHERNET_HEADER_MAX_BYTE;
-    return false;
+        return size <= ETHERNET_HEADER_MAX_BYTE;
+    return size >= ETHERNET_HEADER_MIN_BYTE;
 }
 
-int EthernetHeader::parse(void *buffer, uint32_t size)
+int Ethernet::parse(void *buffer, uint32_t size)
 {
-    JUDGE_RETURN(!check_buffer_length(buffer,size), ETHERNET_PARSE_ERROR_MIN_LENGTH);
+    JUDGE_RETURN(!check_buffer_length(buffer, size), ETHERNET_PARSE_ERROR_MIN_LENGTH);
 
     u_char *skip_buffer = (u_char*)buffer;
-    if (is_have_preamble_and_sfd(buffer, size))
-    {
-        // 指针挪动（前导值+SFD）个字节，指向DST字段
-        skip_buffer += ETHERNET_PREAMBLE_BYTE + ETHERNET_SFD_BYTE;
-        _size = ETHERNET_HEADER_MAX_BYTE;
-    }
+    /*********************************************************************************************************
+     * 以太网帧不包含前导码和帧开始符这些物理层的内容
+     * 数据区域是以太网帧的有效载荷部分开始，也就是DST目标MAC地址开始
+     * 所以以下处理无任何意义！！！！！！！！！！！！！！！！！！！！！！！！
+     * *****************************************************************************************************/ 
+    //if (is_have_preamble_and_sfd(buffer, size))
+    //{
+    //    // 指针挪动（前导值+SFD）个字节，指向DST字段
+    //    skip_buffer += ETHERNET_PREAMBLE_BYTE + ETHERNET_SFD_BYTE;
+    //    _size = ETHERNET_HEADER_MAX_BYTE;
+    //}
 
     memcpy(&(_target_mac), skip_buffer, ETHERNET_MAC_BYTE); 
     // 移动skip_buffer,指向SRC字段
@@ -70,105 +89,82 @@ int EthernetHeader::parse(void *buffer, uint32_t size)
     memcpy(&(_source_mac), skip_buffer, ETHERNET_MAC_BYTE); 
     skip_buffer += ETHERNET_MAC_BYTE;
     memcpy(&(_type), skip_buffer, sizeof(_type));
+    skip_buffer += sizeof(_type);
     // 因为以太网数据包的字节序为网络字节序
     // _type整数字节序 与 ethernet.h定义的协议号类型相反
     // 需要反转字节序
-    _type = OPPOSIZE_SHORT_ORDER(_type);
+    //_type = OPPOSIZE_SHORT_ORDER(_type);
 
     _is_opposite_byte ? opposite_byte() : 1;
-    return is_protocol_type() ? PARSE_SUCCESS : ETHERNET_PARSE_ERROR_PROTOCOL_TYPE;
+    //return is_protocol_type() ? PARSE_SUCCESS : ETHERNET_PARSE_ERROR_PROTOCOL_TYPE;
+    
+    int error_code = PARSE_SUCCESS;
+    switch(_type)
+    {
+        case ETHERTYPE_IP:
+            error_code = _data.parse(skip_buffer, size - (skip_buffer - (u_char*)buffer));
+            
+            //printf("尝试输出ip数据包信息：\n");
+            //_data.debug_info();
+            //sleep(2);
+            /************************************************************************************************/
+            // _data是Protocol类（虚基类），使其指针指向Ip对象，后续_data的实际对象是Ip类
+            //_data = new Ip();
+            //if (_data != nullptr)
+            //{
+            //    error_code = _data->parse(skip_buffer, size - (skip_buffer - (u_char*)buffer));
+            //    printf("尝试输出ip数据包信息：\n");
+            //    _data->debug_info();
+            //    sleep(2);
+            //}
+            break;
+        case ETHERTYPE_ARP:
+            //return true;
+        case ETHERTYPE_REVARP:
+        case ETHERTYPE_VLAN:
+        case ETHERTYPE_IPV6:
+        case ETHERTYPE_LOOPBACK:
+            break;
+        default:
+            break;
+    }
+    return error_code;
 }
 
-int EthernetHeader::opposite_byte()
+int Ethernet::opposite_byte()
 {
+    // 需要和ethernet.h定义协议类型作比较，需要把小端字节序转换
+    // 比如和0x0800比较 
     _type = OPPOSIZE_SHORT_ORDER(_type);
     return 0;
 }
 
-int EthernetHeader::debug_info()
+int Ethernet::debug_info()
 {
     /* 默认不输出前导符和帧开始符 */
     auto print_byte = [](u_char* buf, int size) {
         for (int index = 1; index < size; index++)
             printf("-%02X",buf[index]);
     };
+    printf(", source_mac = %02X",_source_mac[0]);
+    print_byte(_source_mac, ETHERNET_MAC_BYTE);
     printf("target_mac = %02X", _target_mac[0]);
     print_byte(_target_mac, ETHERNET_MAC_BYTE);
 
-    printf(", source_mac = %02X",_source_mac[0]);
-    print_byte(_source_mac, ETHERNET_MAC_BYTE);
-
-    switch(_type)
-    {
-        case ETHERTYPE_IP:
-            printf(" protocol = %s", "IPV4");
-        case ETHERTYPE_ARP:
-            printf(" protocol = %s", "ARP");
-        case ETHERTYPE_REVARP:
-            printf(" protocol = %s", "Reverse ARP");
-        case ETHERTYPE_AT:
-            printf(" protocol = %s", "AppleTalk");
-        case ETHERTYPE_AARP:
-            printf(" protocol = %s", "AppleTalk ARP");
-        case ETHERTYPE_VLAN:
-            printf(" protocol = %s", "VLAN");
-        case ETHERTYPE_IPV6:
-            printf(" protocol = %s", "IPV6");
-        case ETHERTYPE_LOOPBACK:
-            printf(" protocol = %s", "LOOPBACK"); 
-    }
-    printf("\n");
+    
+    printf("   %s", get_network_layer_protocol_name(_type));
+    //sleep(1); 
+    // 输出传输层协议内容
+    _data.debug_info();
+    //if (_data != nullptr)
+    //{
+    //    printf("打印IP数据包之前\n");
+    //    sleep(10);
+    //    _data->debug_info();
+    //}
+    //else 
+        printf("\n");
     return 0;
 }
 
-Ethernet::Ethernet(bool is_opposite_byte)
-    : Protocol(is_opposite_byte)
-    , _header(is_opposite_byte)
-    , _data(nullptr)
-    , _data_size(0)
-{
-    _size = 0;
-}
-
-Ethernet::~Ethernet()
-{
-    _data = nullptr;
-    _data_size = 0;
-}
-
-/*
- * @remark 由于抓包程序可能在设备驱动程序未填充字节前就已捕获，故该函数不调用
- * */
-bool Ethernet::check_buffer_length(void *buffer, uint32_t size)
-{
-    return size >= ETHER_MIN_LEN;
-}
-
-/*
- * 解析网络数据包
- * @ return ErrorCode 
- * */
-int Ethernet::parse(void *buffer, uint32_t size)
-{
-    int error_code = _header.parse(buffer, size);
-    JUDGE_RETURN(error_code != PARSE_SUCCESS, error_code);
-    _data = (void*)((u_char*)buffer + _header._size);
-
-    /* 暂时提取剩余缓冲区为数据区域长度 */
-    _data_size = size - _header._size;
-    _size = size;
-    _is_opposite_byte ? opposite_byte() : 1;
-    return PARSE_SUCCESS;
-}
-
-int Ethernet::opposite_byte()
-{
-    return 0;
-}
-
-int Ethernet::debug_info()
-{
-    _header.debug_info();
-    return 0;
-}
 
--- protocol_parse/ethernet.h
+++ protocol_parse/ethernet.h
@@ -3,7 +3,7 @@
 
 #include "base.h"
 #include "protocol.h"
-
+#include "ip.h"
 /** 暂时默认解析的以太网数据包无帧校验序列fcs字段 **/
 
 #define ETHERNET_MAC_BYTE               6       // 以太网数据包MAC地址字节长度
@@ -20,7 +20,7 @@
 #define ETHERNET_MIN_BYTE               60      // 以太网数据包最小长度，去除前导符和帧开始符
 #define ETHERNET_MAX_BYTE               1522    // 以太网数据包最大长度,不计入帧检验序列FCS字段
 
-class EthernetHeader : public Protocol 
+class Ethernet : public Protocol 
 {
 public:
     u_char _preamble[ETHERNET_PREAMBLE_BYTE];   // 前导码
@@ -28,9 +28,11 @@ public:
     u_char _target_mac[ETHERNET_MAC_BYTE];      // 目的MAC地址 
     u_char _source_mac[ETHERNET_MAC_BYTE];      // 源MAC地址 
     uint16_t _type;                             // 类型字段，确定上层的协议
-
-    EthernetHeader(bool is_opposite_byte);
-    ~EthernetHeader();
+    //Protocol *_data;                            // 数据部分
+    Ip _data;
+    Ethernet();
+    ~Ethernet();
+    //Ethernet(const Ethernet&) = delete;
     
     // 是否有前导码和帧开始符
     bool is_have_preamble_and_sfd(void *buffer, uint32_t size);
@@ -42,23 +44,6 @@ public:
     int parse(void *buffer, uint32_t size);
     int opposite_byte();
     int debug_info();
-};
-
-class Ethernet : public Protocol 
-{
-public:
-    EthernetHeader _header;     // 以太网数据包头
-    void *_data;                 // 以太网数据包数据区
-    uint32_t _data_size;        // 数据包中数据长度
-
-    Ethernet(bool is_opposite_byte);
-    ~Ethernet();
-    
-    bool check_buffer_length(void *buffer, uint32_t size);
-    int parse(void *buffer, uint32_t size);
-    int opposite_byte();
-    int debug_info();
-
 };
 
 #endif 
