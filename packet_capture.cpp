f8317da2ddcc76097b324bc81ae6beb8ce5ab30f
yhs-coder <yhs6666@outlook.com>
2024-04-21 23:49:23 +0800

    测试dns

--- packet_capture.cpp
+++ packet_capture.cpp
@@ -24,12 +24,59 @@ void process_packet(u_char *arg, const struct pcap_pkthdr *pkthdr, const u_char 
     // 打印抓取的数据包长度和实际发送的数据包长度
     // std::cout << "caplen: " << pkthdr->caplen << "\t len: " << pkthdr->len << std::endl;
     TimeStamp ts;
-    printf("%d\t", PacketCapture::_num);
-    ts.show_time(pkthdr);
+    // printf("%d\t", PacketCapture::_num);
+    // ts.show_time(pkthdr);
 
     EthernetII ethernet(packet, ts.caplen(pkthdr));
-    std::cout << ethernet.type_string() << std::endl;
+    // std::cout << ethernet.type_string() << "  ";
+    // if (ethernet.is_ipv6())
+    // {
+    //     std::cout << "This is ipv6 packet" << std::endl;
+    // }
+    if (ethernet.is_ipv4())
+    {
+        IPv4Packet ip(ethernet.payload(), ethernet.payload_size());
+        ip.parse();
+        // std::cout << "source ip: " << ip.source_ip().to_string() << "  target ip: " << ip.target_ip().to_string()
+        // << " ";
+        // << to_string(ip.protocol_type()) << "\n";
+        // ip.debuf_info();
+        switch (ip.protocol_type())
+        {
+        // case ProtocolType::ICMP:
+        //     printf("ICMP\n");
+        //     break;
+        // case ProtocolType::TCP:
+        //     printf("TCP\n");
+        //     break;
+        case ProtocolType::UDP:
+        {
+            UdpPacket udp(ip.payload(), ip.payload_size());
+            udp.parse();
+
+            // printf("%d\t", PacketCapture::_num);
+            // ts.show_time(pkthdr);
+            // std::cout << ethernet.type_string() << "  ";
+            // std::cout << "source ip: " << ip.source_ip().to_string() << "  target ip: " << ip.target_ip().to_string()
+            //           << " ";
+            // std::cout << to_string(ip.protocol_type()) << " source_port: " << udp.source_port() << " target port: "
+            //           << udp.target_port() << std::endl;
+
+            // udp.debug_info();
+            if (udp.is_dns())
+            {
+                DnsView dns(udp.payload(), udp.payload_size());
+                dns.debug_info();
+            }
+        }
+        break;
+        default:
+            break;
+        }
+    }
     // ethernet.debug_info();
+    /*
+    // 测试arp数据包
     if (ethernet.is_arp())
     {
         std::cout << "==============================================" << std::endl;
@@ -61,13 +108,7 @@ void process_packet(u_char *arg, const struct pcap_pkthdr *pkthdr, const u_char 
         }
         // arp.debug_info();
         PacketCapture::arp_map.emplace(arp.source_ip(), arp.format_mac_address(arp.source_mac()));
-    }
-    // std::cout << "arp映射： IP地址  MAC地址" << std::endl;
-    // // for (auto &[ip, mac] : arp_map) //结构化绑定 C++17
-    // for (const auto &pair : arp_map)
-    // {
-    //     std::cout << pair.first.to_string() << pair.second.c_str() << std::endl;
-    // }
+    }*/
     // sleep(1);
 }
 
